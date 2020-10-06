e8272eedda6a25f5e7471ec3088268e55d88d2bc
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-05 23:53:45 -0400

    Turned debug off

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['polls-jp5474.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
