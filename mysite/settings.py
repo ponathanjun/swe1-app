ee8c3a0c957a2470b74bd95020971ef03c4ea271
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-06 00:21:14 -0400

    Testing if secret_key was the issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm!im+50u57ot6#eze$2l%t-t#n9xd-)59j9(63*c7_tp@1_p4m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
