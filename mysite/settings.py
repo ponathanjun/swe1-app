e2f87c25b565e4d2d27f94d6142e594a0dad68df
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-06 00:30:02 -0400

    Secret key was the problem, fixing with static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
